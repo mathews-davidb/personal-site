0d2f4cb759c7c4eb8427486fbc30b0ae22604811
mathews-davidb <mathews.davidb@gmail.com>
2021-11-29 21:01:35 -0600

    trying to push

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -6,6 +6,21 @@ import "../Style.css";
 const Nav = () => {
   return (
     <nav>
+      <a href="https://www.linkedin.com/in/davidb-mathews/">
+        <img
+          class="social"
+          src={"images/linkedIn_PNG38.png"}
+          alt="Linkedin"
+          style={{
+            height: "35px",
+            width: "auto",
+            marginLeft: "2em",
+            marginTop: "0.9em",
+            marginRight: "1em",
+          }}
+        />
+      </a>
+
       {navLinks.map(({ navLinkId, scrollToId }, idx) => (
         <NavLink key={idx} navLinkId={navLinkId} scrollToId={scrollToId} />
       ))}
